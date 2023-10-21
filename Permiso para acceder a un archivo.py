01857f95996791ee1ae1d4670796b541a4bd15d7
Mila <115313493+Milamagof@users.noreply.github.com>
2023-10-21 20:19:01 +0200

    Add files via upload

---
+++ Permiso para acceder a un archivo.py
@@ -0,0 +1,17 @@
+"""Función relacionada con el análisis de los intentos de inicio de sesión. En función de la información
+ que recibe, esta función calcula el porcentaje de intentos fallidos y devuelve un porcentaje.
+ El programa podría usarse para determinar si se debe bloquear o no a una cuenta"""
+
+
+def calcular_errores(intentos_totales, intentos_fallidos):
+    porcentaje_errores = intentos_fallidos / intentos_totales
+    return porcentaje_errores
+
+porcentaje= calcular_errores(4,2)
+
+if (porcentaje >= .5):
+    print("Cuenta bloqueada")
+
+
+print(porcentaje)
+
